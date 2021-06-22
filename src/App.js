b0f9179768c5094077db9c44055e13c2eb401ffb
su <suhaib.z.ahmed@gmail.com>
2021-06-22 17:28:29 +0530

    whatsapp clone completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import React, { useEffect, useState } from 'react';
+import Pusher from "pusher-js"
+import axios from "./axios"
 
 function App() {
+
+  const [messages, setMessages] = useState([])
+
+  // when app loads, run this once
+  useEffect(() => {
+    axios.get("/messages/sync")
+    .then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
+  // new message effect
+  useEffect(() => {
+    const pusher = new Pusher('ec4f3ea4626953853beb', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');   // "messages" getting from server.js
+    channel.bind('inserted', (newMessage) => {    // "inserted" getting from server.js
+      setMessages([...messages, newMessage])
+    }); 
+
+    // clean-up function after sending message
+    return () => {  
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages])
+
+  console.log(messages)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat messages={messages}/>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-BWf3cNFI3GDm8xNqnrl8uC6IXwZPdyE",
+    authDomain: "whatsapp-clone-88ba4.firebaseapp.com",
+    projectId: "whatsapp-clone-88ba4",
+    storageBucket: "whatsapp-clone-88ba4.appspot.com",
+    messagingSenderId: "790046558117",
+    appId: "1:790046558117:web:e16e73d47f9353d12c0a4c"
+  };
